59a6b90053f8600ce3b912445adb02cd8d215238
cats <cats@pop-os.localdomain>
2023-07-16 19:43:48 -0600

    added functions to check who wins comparing indexes of arrayGrid to player markers

--- ap.js
+++ ap.js
@@ -3,12 +3,15 @@ const arrayGrid = []
 
 let grids = document.querySelectorAll('.grid')
 
-let marker = 'O'
+let marker = ''
 let click = function (e){
     turn();
     e.target.textContent = marker
     console.log(player1.turn)
+    assingValueToArray();
     e.target.removeEventListener('click', click)
+    player1Wins()
+    player2Wins()
     
 }
 grids.forEach((grid)=>{
@@ -17,7 +20,6 @@ grids.forEach((grid)=>{
 })
 
 
-let one = document.querySelector('.one')
 
 let Player = function(player, marker, turn, score){
     this.player = player;
@@ -42,6 +44,78 @@ const turn = ()=>{
         marker = player2.marker;
         player1.turn = true;
     }
-   
-    
 }
+
+//asignar valor de cada grid a su equivalente en el arrayGrid[]
+const assingValueToArray = ()=>{
+    let grid1 = document.querySelector('.one')
+    let grid2 = document.querySelector('.two')
+    let grid3 = document.querySelector('.three')
+    let grid4 = document.querySelector('.four')
+    let grid5 = document.querySelector('.five')
+    let grid6 = document.querySelector('.six')
+    let grid7 = document.querySelector('.seven')
+    let grid8 = document.querySelector('.eight')
+    let grid9 = document.querySelector('.nine')
+
+    arrayGrid[0] = grid1.textContent
+    arrayGrid[1] = grid2.textContent
+    arrayGrid[2] = grid3.textContent
+    arrayGrid[3] = grid4.textContent
+    arrayGrid[4] = grid5.textContent
+    arrayGrid[5] = grid6.textContent
+    arrayGrid[6] = grid7.textContent
+    arrayGrid[7] = grid8.textContent
+    arrayGrid[8] = grid9.textContent
+}
+
+//condicion de ganar
+
+const player1Wins = ()=>{
+    if(arrayGrid[0] == 'O'&& arrayGrid[1] == 'O' && arrayGrid[2] == 'O'){
+        console.log('player 1 wins')
+    }else if (arrayGrid[3] == 'O'&& arrayGrid[4] == 'O' && arrayGrid[5] == 'O'){
+        console.log('player 1 wins')
+
+    }else if (arrayGrid[6] == 'O'&& arrayGrid[7] == 'O' && arrayGrid[8] == 'O'){
+        console.log('player 1 wins')
+
+    }else if (arrayGrid[0] == 'O'&& arrayGrid[3] == 'O' && arrayGrid[6] == 'O'){
+        console.log('player 1 wins')
+
+    }else if (arrayGrid[1] == 'O'&& arrayGrid[4] == 'O' && arrayGrid[7] == 'O'){
+        console.log('player 1 wins')
+
+    }else if (arrayGrid[2] == 'O'&& arrayGrid[5] == 'O' && arrayGrid[8] == 'O'){
+        console.log('player 1 wins')
+
+    }else if (arrayGrid[6] == 'O'&& arrayGrid[4] == 'O' && arrayGrid[2] == 'O'){
+        console.log('player 1 wins')
+
+    }
+}
+
+const player2Wins = ()=>{
+    if(arrayGrid[0] == 'X'&& arrayGrid[1] == 'X' && arrayGrid[2] == 'X'){
+        console.log('player 2 wins')
+    }else if (arrayGrid[3] == 'X'&& arrayGrid[4] == 'X' && arrayGrid[5] == 'X'){
+        console.log('player 2 wins')
+
+    }else if (arrayGrid[6] == 'X'&& arrayGrid[7] == 'X' && arrayGrid[8] == 'X'){
+        console.log('player 2 wins')
+
+    }else if (arrayGrid[0] == 'X'&& arrayGrid[3] == 'X' && arrayGrid[6] == 'X'){
+        console.log('player 2 wins')
+
+    }else if (arrayGrid[1] == 'X'&& arrayGrid[4] == 'X' && arrayGrid[7] == 'X'){
+        console.log('player 2 wins')
+
+    }else if (arrayGrid[2] == 'X'&& arrayGrid[5] == 'X' && arrayGrid[8] == 'X'){
+        console.log('player 2 wins')
+
+    }else if (arrayGrid[6] == 'X'&& arrayGrid[4] == 'X' && arrayGrid[2] == 'X'){
+        console.log('player 2 wins')
+
+    }
+}
+
