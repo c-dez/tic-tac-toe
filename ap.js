540a225e05d4bc2d01b08b009b7f0d946b63701c
c-dez <hdezcruzcarlos@gmail.com>
2023-07-17 12:07:43 -0600

    restored old version

--- ap.js
+++ ap.js
@@ -47,3 +47,74 @@ const turn = ()=>{
    
     
 }
+const assingValueToArray = ()=>{
+    let grid1 = document.querySelector('.one')
+    let grid2 = document.querySelector('.two')
+    let grid3 = document.querySelector('.three')
+    let grid4 = document.querySelector('.four')
+    let grid5 = document.querySelector('.five')
+    let grid6 = document.querySelector('.six')
+    let grid7 = document.querySelector('.seven')
+    let grid8 = document.querySelector('.eight')
+    let grid9 = document.querySelector('.nine')
+
+    arrayGrid[0] = grid1.textContent
+    arrayGrid[1] = grid2.textContent
+    arrayGrid[2] = grid3.textContent
+    arrayGrid[3] = grid4.textContent
+    arrayGrid[4] = grid5.textContent
+    arrayGrid[5] = grid6.textContent
+    arrayGrid[6] = grid7.textContent
+    arrayGrid[7] = grid8.textContent
+    arrayGrid[8] = grid9.textContent
+}
+
+//condicion de ganar
+
+const player1Wins = ()=>{
+    if(arrayGrid[0] == 'O'&& arrayGrid[1] == 'O' && arrayGrid[2] == 'O'){
+        console.log('player 1 wins')
+    }else if (arrayGrid[3] == 'O'&& arrayGrid[4] == 'O' && arrayGrid[5] == 'O'){
+        console.log('player 1 wins')
+
+    }else if (arrayGrid[6] == 'O'&& arrayGrid[7] == 'O' && arrayGrid[8] == 'O'){
+        console.log('player 1 wins')
+
+    }else if (arrayGrid[0] == 'O'&& arrayGrid[3] == 'O' && arrayGrid[6] == 'O'){
+        console.log('player 1 wins')
+
+    }else if (arrayGrid[1] == 'O'&& arrayGrid[4] == 'O' && arrayGrid[7] == 'O'){
+        console.log('player 1 wins')
+
+    }else if (arrayGrid[2] == 'O'&& arrayGrid[5] == 'O' && arrayGrid[8] == 'O'){
+        console.log('player 1 wins')
+
+    }else if (arrayGrid[6] == 'O'&& arrayGrid[4] == 'O' && arrayGrid[2] == 'O'){
+        console.log('player 1 wins')
+
+    }//falta 0 4 8
+}
+
+const player2Wins = ()=>{
+    if(arrayGrid[0] == 'X'&& arrayGrid[1] == 'X' && arrayGrid[2] == 'X'){
+        console.log('player 2 wins')
+    }else if (arrayGrid[3] == 'X'&& arrayGrid[4] == 'X' && arrayGrid[5] == 'X'){
+        console.log('player 2 wins')
+
+    }else if (arrayGrid[6] == 'X'&& arrayGrid[7] == 'X' && arrayGrid[8] == 'X'){
+        console.log('player 2 wins')
+
+    }else if (arrayGrid[0] == 'X'&& arrayGrid[3] == 'X' && arrayGrid[6] == 'X'){
+        console.log('player 2 wins')
+
+    }else if (arrayGrid[1] == 'X'&& arrayGrid[4] == 'X' && arrayGrid[7] == 'X'){
+        console.log('player 2 wins')
+
+    }else if (arrayGrid[2] == 'X'&& arrayGrid[5] == 'X' && arrayGrid[8] == 'X'){
+        console.log('player 2 wins')
+
+    }else if (arrayGrid[6] == 'X'&& arrayGrid[4] == 'X' && arrayGrid[2] == 'X'){
+        console.log('player 2 wins')
+
+    }//falta 0 4 8
+}
